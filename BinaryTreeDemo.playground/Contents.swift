70c6c72162510a900890b199601e3c907fbb7b05
Prashanth Moorthy <prashanth.moorthy@sap.com>
2017-02-16 18:12:34 +0530

    Added Insert and Search Functions

--- BinaryTreeDemo.playground/Contents.swift
+++ BinaryTreeDemo.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 // ========================= BTNode ========================= //
 
-class BTNode {
+class BTNode:CustomStringConvertible {
     
     var key : Int = -1                // Player Jersery #
     var value : String = ""           // Player Name
@@ -29,8 +29,10 @@ class BTNode {
         return rightChildNode != nil
     }
     
+    var description: String {
+        return "\n===========================\n\t\t\(self.key) \n\t ⋰  ⋱  \n   \(leftChildNode?.key ?? -1) \t  \(rightChildNode?.key ?? -1)   \n\n \n\t\t\(self.value) \n\t  \n   \(leftChildNode?.value ?? "nil") \t  \(rightChildNode?.value ?? "nil") \n==========================="
+    }
 }
-
 
 
 // ========================================================== //
@@ -100,6 +102,76 @@ class BTree{
         rootNode?.rightChildNode = nil
         
     }
+    
+    //MARK: Searching
+    func find(_ key : Int) -> BTNode? {
+        if rootNode == nil {return nil}
+        return lookUp(key)
+    }
+    
+    
+    private func lookUp(_ key : Int, startingNode : BTNode? = nil) -> BTNode? {
+        
+        let tempWorkNode = startingNode ?? rootNode!
+        
+        if tempWorkNode.key == key{return tempWorkNode}
+        
+        if tempWorkNode.key < key {
+            if tempWorkNode.hasRightNode{
+                return lookUp(key, startingNode: tempWorkNode.rightChildNode)
+            }
+            return nil
+        }
+        
+        if tempWorkNode.key > key {
+            if tempWorkNode.hasLeftNode{
+                return lookUp(key, startingNode: tempWorkNode.leftChildNode)
+            }
+            return nil
+        }
+        
+        return nil
+    }
+
 
 }
 
+
+//: Build the tree.
+let playerBTree = BTree()
+
+//  44	Asiata, Matt
+//  55	Barr, Anthony
+//  64	Beavers, Willie
+//  61	Berger, Joe
+//  76	Boone, Alex
+//  8   Bradford, Sam
+//  40	Brothers, Kentrell
+//  68	Clemmings, T.J.
+//  57	Cole, Audie
+//  14	Diggs, Stefon
+//  2   Forbath, Kai
+//  6   Heinicke, Taylor
+//  69	Hill, Rashod
+
+playerBTree.insertNode(inputNode: BTNode(key: 44, value: "Asiata, Matt"))
+playerBTree.insertNode(inputNode: BTNode(key: 55, value: "Barr, Anthony"))
+playerBTree.insertNode(inputNode: BTNode(key: 64, value: "Beavers, Willie"))
+playerBTree.insertNode(inputNode: BTNode(key: 61, value: "Berger, Joe"))
+playerBTree.insertNode(inputNode: BTNode(key: 8, value: "Bradford, Sam"))
+playerBTree.insertNode(inputNode: BTNode(key: 40, value: "Brothers, Kentrell"))
+playerBTree.insertNode(inputNode: BTNode(key: 68, value: "Clemmings, T.J."))
+playerBTree.insertNode(inputNode: BTNode(key: 57, value: "Cole, Audie"))
+playerBTree.insertNode(inputNode: BTNode(key: 14, value: "Diggs, Stefon"))
+playerBTree.insertNode(inputNode: BTNode(key: 2, value: "Forbath, Kai"))
+playerBTree.insertNode(inputNode: BTNode(key: 6, value: "Heinicke, Taylor"))
+playerBTree.insertNode(inputNode: BTNode(key: 69, value: "Hill, Rashod"))
+
+// Search for a key present in the tree.
+
+let resultNode = playerBTree.find(14)
+print(resultNode ?? "Node Not found")
+
+
+let badNode = playerBTree.find(99)
+print(badNode ?? "Node Not found")
